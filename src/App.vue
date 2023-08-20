bdcd4d8d76e6d4ae87c2f7212034dac82758c9bb
Krotesk <igor_98_zukov@mail.ru>
2023-08-20 18:59:24 +0300

    Пагинация

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,19 @@
 			v-if="!isPostsLoading"
 		/>
 		<div v-else> Идет загрузка...</div>
+		<div class="page__wrapper">
+			<div 
+				v-for="pageNumber in totalPages" 
+				:key="pageNumber" 
+				class="page"
+				:class="{
+					'page_current': page === pageNumber
+				}"
+				@click="changePage(pageNumber)"
+			> 
+				{{ pageNumber }}
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -33,7 +46,6 @@
 import PostList from "@/components/PostList"
 import PostForm from "@/components/PostForm"
 import axios from "axios"
-import { watch } from "vue"
 export default {
 	components:{
 		PostForm, PostList
@@ -44,6 +56,9 @@ export default {
 			dialogVisible: false,
 			isPostsLoading:false,
 			selectedSort:"",
+			page: 1,
+			limit:10,
+			totalPages:0,
 			sortOptions:[
 				{value: "title", name:"По названию"},
 				{value: "body", name:"По описанию"}
@@ -62,10 +77,20 @@ export default {
 		showDialog(){
 			this.dialogVisible = true
 		},
+		changePage(number){
+			this.page = number,
+			this.fetchPosts()
+		},
 		async fetchPosts() {
 			try {
 				this.isPostsLoading = true;
-				const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+				const response = await axios.get('https://jsonplaceholder.typicode.com/posts',{
+					params:{
+						_page: this.page,
+						_limit:10
+					}
+				});
+				this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
 				this.posts = response.data;
 			} catch (error) {
 				alert (error)
@@ -103,5 +128,23 @@ export default {
 	display: flex;
 	justify-content: space-between;
 	margin-top:20px;
+}
+.page__wrapper{
+	display: flex;
+	gap: 10px;
+	margin: 10px;
+}
+.page{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border:  1px solid greenyellow;
+	width: 25px;
+	height: 25px;
+	cursor: pointer;
+}
+.page_current{
+	background-color: greenyellow;
+	color: rgb(0, 54, 31);
 }
 </style>
